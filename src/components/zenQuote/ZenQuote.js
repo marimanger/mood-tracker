b7be2983c4dfba981c220e25769c47f86a7e7aff
Mariia <mariiamanger@gmail.com>
2022-08-19 12:20:08 +0100

    quote api replacement

--- src/components/zenQuote/ZenQuote.js
+++ src/components/zenQuote/ZenQuote.js
@@ -4,29 +4,30 @@ import axios from "axios";
 
 function ZenQuote() {
   const [quote, setQuote] = useState({
+    quote: "",
     author: "",
-    text: "",
   });
 
-  const GET_QUOTES = `https://type.fit/api/quotes`;
+  const GET_QUOTES = `https://api.goprogram.ai/inspiration`;
   const getQuote = async () => {
     try {
       const response = await axios.get(GET_QUOTES);
       // console.log("Quotes data", response.data);
-      getOnequoteRandom(response.data);
+      setQuote(response.data);
     } catch (error) {
       console.log("error", error);
     }
   };
 
-  function getOnequoteRandom(data) {
-    const randomQuote = Math.floor(Math.random() * data.length);
-    console.info(`Random quote is ${JSON.stringify(data[randomQuote])}`);
-    setQuote({
-      author: data[randomQuote].author,
-      text: data[randomQuote].text,
-    });
-  }
+  //Random Quote data
+  // function getOnequoteRandom(data) {
+  //   const randomQuote = Math.floor(Math.random() * data.length);
+  //   console.info(`Random quote is ${JSON.stringify(data[randomQuote])}`);
+  //   setQuote({
+  //     quote: data[randomQuote].author,
+  //     author: data[randomQuote].text,
+  //   });
+  // }
 
   useEffect(() => {
     getQuote();
@@ -37,7 +38,7 @@ function ZenQuote() {
       <section className="quote__section">
         <h2 className="quote__header">Inspiratonal quote for you:</h2>
         <div className="quote__wrapper">
-          <p className="quote__text">"{quote.text}"</p>
+          <p className="quote__text">"{quote.quote}"</p>
           <p className="quote__author">{quote.author}</p>
         </div>
       </section>
